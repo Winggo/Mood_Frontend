0def892815604070ffa6e4d551f6b1dd3b5ed080
winggo <winggo.tse@gmail.com>
2019-12-21 20:39:24 -0800

    inital commit for frontend

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
 
-    <Nav />
+Router.events.on('routeChangeStart', url => {
+  NProgress.start();
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+const onSubmit = (e) => {
+  if(e.keyCode === 13) {
+    e.preventDefault();
+    let query = document.getElementById('searchBox').value;
+    Router.push({
+      pathname: '/results',
+      query: {'q': query}
+    })
+  }
+}
+
+const Home = () => {
+  useEffect(() => {
+    document.getElementById('searchBox').focus();
+  }, [])
+
+  return (
+    <div>
+      <Head>
+        <title>Mood</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      
+      <div className="hero">
+        <h1 className="title">Mood</h1>
+        <p className="description">
+          What's the sentiment of the internet?
+        </p>
+
+        <input type='text' name='search' id='searchBox' 
+          placeholder="Press 'enter' to search" 
+          onKeyDown={(e) => onSubmit(e)}/>
       </div>
-    </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+      <style jsx>{` 
+        .hero {
+          width: 100%;
+          color: #333;
+          padding-top: 30vh;
+          text-align: center;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        #searchBox {
+          margin: 20px;
+          width: 30vw;
+        }
+        #searchBox::placeholder {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Home
---
+++ pages/results.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Results = (props) => {
+    return (
+        <div>
+            <Head>
+                <title>Mood</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div>
+                <Link href='/'>
+                    <a>Mood</a> 
+                </Link>
+                <h1>
+                    {props.query}
+                </h1>
+                <h3>
+                    {props.numTweets} Tweets found
+                </h3>
+                {props.tweets.map((tweet, index) => (
+                    <p key={index}>{tweet}</p>
+                ))}
+                <p>
+                    {props.tweets}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+Results.getInitialProps = async ({query}) => {
+    console.log('http://localhost:5000/' + query['q'])
+    const res = await fetch('http://localhost:5000/' + query['q'], {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await res.json();
+    console.log(data['numTweets'])
+    return {
+        'query': data['query'],
+        'tweets': data['tweets'],
+        'numTweets': data['numTweets'],
+    }
+}
+
+export default Results
